7ed5d2c1f1013f61d27324387f6d2dc98ee106eb
菜 <31943485+13121349269@users.noreply.github.com>
2021-09-27 10:47:38 +0800

    Update CheckAccount_course.py

--- CheckAccount_course.py
+++ CheckAccount_course.py
@@ -9,9 +9,9 @@ import os
 
 # 定义各项目信息
 contacts = "韩静"
-tel = "18910777860"
-#url = "https://web.hopex.com/api/v1/gateway/Home/ContractSummary?market=BTCUSDT&marketCode=BTCUSDT&contractCode=BTCUSDT&lang=cn"
-# url = "https://api0.yuanfangnet.com/v1/api/paycenter/checkapi"
+tel = "xxxxxxx"
+#url = "https://xxxxx.com/api/v1/gateway/Home/ContractSummary?market=BTCUSDT&marketCode=BTCUSDT&contractCode=BTCUSDT&lang=cn"
+# url = "https://xxxx.com/v1/api/paycenter/checkapi"
 program_name = "CheckAccount"
 # 组合信息
 alert = "\n程序名称:" + program_name + "服务器IP：" + "192.168.92.153" +"\n联系人:" + contacts + "\n联系电话:" + tel + "\n手动启动192.168.92.153上相同程序"
@@ -19,8 +19,8 @@ IP="192.168.92.153"
 
 # 钉钉报警
 def dingding(massage):
-    dingding_url = "https://oapi.dingtalk.com/robot/send?access_token=c6939d2a80b7366958072d3b65d07ba5bebd5e741850c0382f4c192c7b3aff87"
-    #dingding_url = "https://oapi.dingtalk.com/robot/send?access_token=1351a9d6409a366fd37c5aa8401e4f8f180cb152e2e05c7989cff3481594cf47"
+    dingding_url = "https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxx"
+    #dingding_url = "https://oapi.dingtalk.com/robot/send?access_token=xxxxxx"
     dingding_headers = {"Content-Type": "application/json"}
     dingding_massage = massage
     dingding_data = {"msgtype": "text", "text": {"content": dingding_massage}}
